fc23e01f8ab6adebfc9fec89657ce13c2a2bd808
gkcsd <kaleg2025@gmail.com>
2021-08-16 18:21:58 +0530

    bodyParser middleware  added and imported three routes

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
+//bring all the routes
+const auth = require("./routes/api/auth");
+const profile = require("./routes/api/profile");
+const questions = require("./routes/api/questions");
 
 //APP
 const app = express();
+
+//Middleware for body-parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //mongoDB configuration
 const db = require("./setup/myurl").mongoURL;
@@ -16,11 +25,16 @@ mongoose
 
 
 //@route    -   GET /
-//@desc     -   a route to home page
+//@desc     -   just for testing
 //@access   -   PUBLIC
 app.get("/", (req,res) => {
     res.send("Hello World!");
 });
+
+//actual routes
+app.use("/api/auth", auth);
+app.use("/api/profile", profile);
+app.use("/api/questions", questions);
 
 //PORT
 const port = process.env.PORT || 3000;
