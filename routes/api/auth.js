bd6c688a1748007c0531170623dfc9218c0f8ea5
gkcsd <kaleg2025@gmail.com>
2021-08-18 21:34:47 +0530

    person shema imported

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const jsonwt = require('jsonwebtoken');
+const passport = require("passport");
+const key = require("../../setup/myurl");
 
-router.get("/", (req, res) => res.json({test: "Auth is success"}));
+//@type     -   GET
+//@route    -   /
+//@desc     -   just for testing
+//@access   -   PUBLIC  
+router.get("/", (req, res) => res.json({test: "Auth is being tested.."}));
+
+//Import Schema for Person to Register
+const Person = require("../../models/Person");
+
+
 
 module.exports = router;
